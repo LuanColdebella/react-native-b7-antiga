b9ead7d259ef5635cc2845250b886388826aa863
LuanColdebella <luanarabuta@gmail.com>
2025-08-28 21:08:23 -0300

    tela de login

--- App.tsx
+++ App.tsx
@@ -1,57 +1,90 @@
-import { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput } from 'react-native';
+import React, { useState } from 'react';
+import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View
+} from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
-function App() {
+const App = () => {
 
-  const [name, setName] = useState(''); //nome final aqui
-  const [nameField, setNameField] = useState(''); //digitado no campo
+  const [emailField, setEmailField] = useState<string>('');
+  const [passwordField, setPasswordField] = useState<string>('');
 
-  const handleClear = () => {
-      setName('');
+  const hendleLoginBtn = () => {
+    alert(`email: ${emailField} \n senha: ${passwordField}`);
   }
-
-  const handleSendName = () => {
-    setName(nameField);
-    setNameField('')
-  }
-
-
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
+      <SafeAreaView style={{ flex: 1 }}>
 
+      <ScrollView style={styles.scrollView}>
+        <View style={styles.container}>
+          <Image 
+            style={styles.logo}
+            source={require('./assets/logo.png')}
+          />
 
-        {name === '' &&
-          <> {/*FRAGMENT*/}
-            <Text style={styles.title}>Qual o seu nome?</Text>
-            <TextInput 
-              style={{ backgroundColor: '#EEE', padding: 10}}
-              placeholder='Digite seu nome'
+          <Text style={styles.h1}>Sistema de Login</Text>
+          <Text style={styles.h2}>Bem vindo(a)! Digite seus dados abaixo.</Text>
+          
+          <View style={styles.inputArea}>
+            <Text style={styles.inputLabel}>Email</Text>
+            <TextInput
+              style={styles.inputField}
+              placeholder='Digite seu email'
               placeholderTextColor='#999'
-              value={nameField}
-              onChangeText={text => setNameField(text)}
+              value={emailField}
+              onChangeText={(email) => setEmailField(email)}
+              autoCapitalize='none' //nunca deixa a primeira letra maiuscula
+              keyboardType='email-address' //deixa o teclado de email
             />
-            <Button
-              title='Enviar'
-              onPress={handleSendName}
+          </View>
+
+          <View style={styles.inputArea}>
+            <Text style={styles.inputLabel}>Senha</Text>
+            <TextInput
+              style={styles.inputField}
+              placeholder='Digite sua senha'
+              secureTextEntry={true}
+              placeholderTextColor='#999'
+              value={passwordField}
+              onChangeText={(password) => setPasswordField(password)}
             />
-          </>
-        }
-        {name !== '' &&
+          </View>
 
-          <>
-            <Text>Olá {name}, tudo bem?</Text>
-            <Text>Seja bem vindo(a)!</Text>
+          <View style={styles.aditionals}>
+              <TouchableOpacity style={styles.forgotBtnArea}>
+                <Text style={styles.forgotBtnText}>Esqueci minha senha</Text>
+              </TouchableOpacity>
+          </View>
 
-            <Button
-              title={`Não sou ${name}`}
-              onPress={handleClear}
-            />
-          </>
+          <TouchableOpacity 
+            style={styles.button}
+            onPress={() => hendleLoginBtn()}
+            >
+            <Text style={styles.buttonText}>Entrar</Text>
+          </TouchableOpacity>
 
-        }
-       
+          <View style={styles.signUpArea}>
+            <Text style={styles.signUpText}>Não tem uma conta?</Text>
+            <TouchableOpacity>
+              <Text style={styles.signUpBtnText}>Cadastre-se</Text>
+            </TouchableOpacity>
+          </View>
+          
+          <View style={styles.footerArea}>
+            <Text style={styles.footerText}>Criado por Luan Coldebella</Text>
+          </View>
+
+
+        </View>
+      </ScrollView>
+      
 
       </SafeAreaView>
     </SafeAreaProvider>
@@ -60,38 +93,91 @@ function App() {
 
 export default App;
 
+
 const styles = StyleSheet.create({
-  title: {
-    fontSize: 20,
+  scrollView :{
+    backgroundColor: '#FFF', //cor de fundo
+    paddingHorizontal: 15, //espaço na direita e na esquerda
+    paddingTop: 40 // espaço em cima
+  },
+  container: {
+    alignItems: 'center', //alinhar ao centro
+  },
+  logo: {
+    width: 150, //largura
+    height: 150 //altura
+  },
+  h1: {
+    color: '#000', //cor do texto
+    fontSize: 27, //tamnho do texto
+    fontWeight: 'bold', //peso do texto
+    marginVertical: 10 //margem vertical em cima e em baixo
+  },
+  h2: {
+    color: '#999', //cor do texto
+    fontSize: 15, //tamanho do texto
+  },
+  inputArea: {
+    width: '100%', //largura do input 100%
+    paddingTop: 20, //espaco em cima
+
+  },
+  inputLabel: {
+    color: '#777', //cor do texto
+    fontSize: 14, //tamanho do texto
+    fontWeight: 'bold', //peso do texto
+    marginBottom: 7 //margem em baixo
+  },
+  inputField: {
+    borderWidth: 2, //largura da borda
+    borderRadius: 5, //bordas arredondadas qtde
+    borderColor: '#DDD', //cod da borda
+    padding: 10 //espaco interno 
+  },
+  aditionals: {
+    width: '100%' //largura 100%
+  },
+  forgotBtnArea: {
+    paddingVertical: 20, //Colocar este padding para tem um espaço legar para apertar por que o usario pode ter um dedão maior 
+    // backgroundColor: '#FF0000',
+    alignSelf: 'flex-end' //alinhar ele ao lado direito
+  },
+  forgotBtnText: {
+    color: '#4162B7',
+    fontSize: 14,
     fontWeight: 'bold',
-    textAlign: 'center',
-    marginTop: 20,
-    color: 'red'
-  },
-  items: {
-    fontSize: 17,
-    color: '#000',
-    textAlign: 'center'
-  },
-  subTitle: {
-    fontSize: 15,
-    color: '#000',
-    textAlign: 'center',
-    marginTop: 10
   },
   button: {
-    backgroundColor: 'blue',
+    backgroundColor: '#4162B7',
+    width: '100%',
     padding: 10,
-    borderRadius: 5,
-    marginTop: 10,
-    marginHorizontal: 20
+    borderRadius: 5
   },
-  box: {
-    borderColor: '#000000',
-    borderWidth: 1,
-    margin: 20,
-    padding: 10,
-    borderRadius: 5,
-    gap: 10
+  buttonText: {
+    alignSelf: 'center', //alinhar ao centro
+    color: '#FFFF',
+    fontSize: 16,
+  },
+  signUpArea: {
+    flexDirection: 'row', //alinhar lado a lado
+    marginTop: 30,
+  },
+  signUpText: {
+    color: '#999',
+    fontSize: 13,
+    fontWeight: 'bold',
+  },
+  signUpBtnText: {
+    color: '#4162B7',
+    fontSize: 13,
+    fontWeight: 'bold',
+    marginLeft: 5
+  },
+  footerArea: {
+    marginVertical: 30
+  },
+  footerText: {
+    color: '#999',
+    fontSize: 13,
   }
 });
